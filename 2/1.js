608925befe0b328fe20f2e1078515ae323cb71e4
slach <pser@mail.ru>
2018-12-28 19:50:29 +0600

    y-com

--- 2/1.js
+++ 2/1.js
@@ -1,7 +1,2 @@
-qq.gh={};
-qq.gh.github=new Github({username: 'Slachinov', password:'gh22sl04md', auth:'basic'});alert(qq.gh.github);
-var repo=qq.gh.github.getRepo('Slachinov','slachinov.github.io');
-alert(repo);
-function tree(){repo.getTree('master?recursive=true',function(err,tree){window.ttt=tree;alert(err);alert(tree);alert(tree[2]);});};tree();
-qq.gh.write=function(r,n,t){r.write('master',n,t,'y-com',options,function(err){})}};
-
+qq.app.elm=ce(div(),all(),bg('blue'),zi(5),abs(),t(150),fs(35),au(),cl(function(){this.hide()}));
+qq.app.elm.hide()
