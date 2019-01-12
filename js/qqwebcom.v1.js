4166276e6a996b589bdbab84c04299f9f9e4ebea
Slachinov <pser54@mail.ru>
2019-01-12 21:04:53 +0600

    Update qqwebcom.v1.js

--- js/qqwebcom.v1.js
+++ js/qqwebcom.v1.js
@@ -3,7 +3,7 @@ constructor() {
 super();
 let that=this;
 let inp,edit,el,bts;
-el=qq.f.crElem({tag:"div",style:{width:'50%',height:'40%',overflow:'hidden',background:'orange'}, parent:that});
+el=qq.f.crElem({tag:"div",style:{width:'50%',height:'200px',overflow:'hidden',background:'orange'}, parent:that});
 bts=qq.f.crElem({tag:"div",style:{left:'0px',right:'0px',top:'0px',background:'yellow'}, parent:el,it:"localStorage."});
 let h0=el.offsetHeight;let h1=bts.offsetHeight;
 edit=qq.f.crElem({tag:"div",attr:{contentEditable:true},style:{'font-size':'18px',left:'0px',right:'0px',height:(h0-h1)+'px',background:'lightgreen',overflow:'auto'}, parent:el});
@@ -20,5 +20,5 @@ let cl=qq.f.crElem(tmb,{it:'clear',event:{click:function(){edit.innerHTML='';}}}
 let all=qq.f.crElem(tmb,{it:'all',event:{click:function(){edit.setAttribute('contentEditable',false);inp.value='';edit.innerText='';for(let u in localStorage){qq.f.crElem({tag:"div",style:{border:'1px solid black',background:'orange'},prop:{u:u},event:{click:function(){edit.setAttribute('contentEditable',true);inp.value=this.u;edit.innerText=localStorage[this.u]}},it:u,parent:edit});};}}});
 
 //--
-//alert(that.outerHTML);
+alert(that.outerHTML);
 }};
