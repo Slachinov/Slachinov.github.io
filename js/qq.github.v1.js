0110db871af384efc8f75c204fe606979da03710
Slachinov <pser54@mail.ru>
2019-01-13 23:50:05 +0600

    Create qq.github.v1.js

---
+++ js/qq.github.v1.js
@@ -0,0 +1,53 @@
+qq.f.github =function (auth){
+let au=auth||{};
+let gh=new Github(au);
+let user=gh.getUser();
+//======GIST====================
+this.gists=function(){return new Promise(function(resolve, reject) {
+user.gists(function(e,d){if(e)reject(e);if(d)resolve(d)})})};
+//---
+this.usergists =function(un){return new Promise(function(resolve, reject) {
+user.userGists(un,function(e,d){if(e)reject(e);if(d)resolve(d)})})};
+//---
+this.getgist=function(id){return new Promise(function(resolve, reject) {
+let gist=gh.getGist(id);//alert('gist-'+gist);
+gist.read(function(e,d){;if(e)reject(e);if(d)resolve(d)})})};
+//---
+this.creategist=function(){return new Promise(function(resolve, reject) {var gist=gh.getGist('');
+let ars=arguments;
+var val={
+          "description": "local base as localStorage",
+          "public": true,
+          "files": {}
+          };val["files"][name]={};val["files"][name]['content']=txt;
+val["files"][name]['language']="javascript";
+gist.create(val,function(e,d){
+if(e)reject(e);if(d)resolve(d);})})};
+//----------
+this.deletegist=function(id){return new Promise(function(resolve, reject) {var gist=gh.getGist(id);//alert(id);alert(gist.read);
+gist.delete(function(e,d){
+if(e)reject(e);if(d)resolve(d);})})};
+//----------
+this.updategist=function(id,o){return new Promise(function(resolve, reject) {var gist=gh.getGist(id);//alert(id);alert(gist);
+gist.update(o,function(e,d){
+if(e)reject(e);if(d)resolve(d);})})};
+//=========REPOS===============
+this.repos =function(un){return new Promise(function(resolve, reject) {
+user.repos(un,function(e,d){if(e)reject(e);if(d)resolve(d)})})};
+//---
+this.userrepos =function(un){return new Promise(function(resolve, reject) {
+user.userRepos(un,function(e,d){if(e)reject(e);if(d)resolve(d)})})};
+//------
+this.getrepo=function(a){return new Promise(function(resolve, reject) {
+let repo=gh.getRepo(a);//qq.f.obj(repo,1);
+repo.listBranches(function(e,d){if(e)reject(e);if(d)resolve(d)});
+repo.getTree('master?recursive=true',async function(err,tree){if(err){alert('err='+err);reject(err);};resolve(tree);
+});
+repo.show(function(e,d){//alert(e);qq.f.obj(e,1);alert(d);
+if(e)reject(e);if(d)resolve(d)})})};
+//---
+this.gettree =function(n,r){return new Promise(function(resolve, reject) {
+let repo=gh.getRepo(n,r);//alert(repo);
+repo.getTree('master?recursive=true',function(e,d){if(e)reject(e);if(d)resolve(d)})})};
+//------
+};
