f9d4c20e3ec287520551b2c636b430e8b0847095
slach <pser@mail.ru>
2019-01-22 00:33:17 +0600

    qq file

--- js/qq.webcom.github.js
+++ js/qq.webcom.github.js
@@ -42,7 +42,7 @@ let f=await repo.write(n,edit.innerText);alert(f);
 //--
 }}});
 //======================================
-let cre=qq.f.crElem(tmb,red,{it:'create',event:{click:function(){}}});
+//let cre=qq.f.crElem(tmb,red,{it:'create',event:{click:function(){}}});
 //=========DELETE================
 let del=qq.f.crElem(tmb,red,{it:'del',event:{click:async function(){let repo=gh.getrepo('Slachinov','slachinov.github.io');let n=inp.value;alert(n);
 let f=await repo.remove(n);}}});
