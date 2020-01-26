b4513f7af9463b99b9032f107d6143830c1fc15a
slach <pser@mail.ru>
2020-01-26 21:19:27 +0600

    qq file

---
+++ vb6/js/elements.v0.js
@@ -0,0 +1,20 @@
+{let b=async function(nm){//alert(nm);
+let proj=await qq.reg.get('ind').get(nm);
+
+let f=qq.reg.get('div toggle 1');//alert(f);
+let d=qq.reg.get('panels').elements;//alert(d);
+d.innerHTML='';
+for(let u1 in proj.val)
+{let el=f(u1,d);
+let ff;//alert(u1);
+
+if(u1=='func'){ff=async function(){let o=proj.val[u1];
+let m=qq.reg.get('panels').main;m.innerText=o[this.u]}}else if(u1=='els'){ff=async function(){let ht=proj.val[u1][this.u]['html'];let f=qq.reg.get('htmltotable');let d=qq.reg.get('panels').propertes;f(ht,d);}};//alert(f);
+for(let u in proj.val[u1])
+{let el0=qq.ce({tag:'div',style:{border:'1px solid black',background:'lightgray'},event:{click:ff},it:u,parent:el});el0.u=u;}
+}
+//--
+};
+//====
+qq.reg.set('els',b);
+}
