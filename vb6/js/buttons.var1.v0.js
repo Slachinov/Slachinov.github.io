4643b552cd930c273a1f29d651fad919a57e38ae
slach <pser@mail.ru>
2020-01-29 02:23:30 +0600

    qq file

--- vb6/js/buttons.var1.v0.js
+++ vb6/js/buttons.var1.v0.js
@@ -16,9 +16,8 @@ let sk=await f(ht);if(!sk.par){sk.parent=pn;}else {sk.parent=aa[sk.par];};
 let el=qq.ce(sk);aa[sk.name]=el;let elems=qq.reg.get('es');elems[sk.name]=el;
 }
 }},it:'Run project'});
-qq.ce(a,{event:{click:function(){let mod=qq.reg.get('modal');alert(mod);
-mod.f1();}},it:'new Project'});
-qq.ce(a,{event:{click:function(){qq.reg.get('modal').v='fload';qq.reg.get('modal').show(); qq.reg.get('fmodal1')(this.innerText);qq.reg.get('fmodal2')()}},it:'load Project'});
+qq.ce(a,{event:{click:function(){}},it:'new Project'});
+qq.ce(a,{event:{click:function(){}},it:'load Project'});
 qq.ce(a,{event:{click:async function(){let f= qq.reg.get('tabletohtml');let ht=await f();}},it:'save Project'},r);
 qq.ce(a,{event:{},it:'delete Project'},r);
 };
