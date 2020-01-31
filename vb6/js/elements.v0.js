681d216e0a853a51a6c213f2f7d4670dbdb91687
slach <pser@mail.ru>
2020-01-31 13:49:41 +0600

    qq file

--- vb6/js/elements.v0.js
+++ vb6/js/elements.v0.js
@@ -8,8 +8,8 @@ for(let u1 in proj.val)
 {let el=f(u1,d);
 let ff;//alert(u1);
 
-if(u1=='func'){ff=async function(){let o=proj.val[u1];
-let m=qq.reg.get('panels').edit;m.innerText=o[this.u];m.elem=this;}}else if(u1=='els'){ff=async function(){let ht=proj.val[u1][this.u]['html'];let f=qq.reg.get('htmltotable');let d=qq.reg.get('panels').propertes;f(ht,d);}};//alert(f);
+if(u1=='func'){ff=async function(){let o=proj.val['func'];
+let m=qq.reg.get('panels').edit;m.innerText=o[this.u];m.elem=this;m.nm=this.u}}else if(u1=='els'){ff=async function(){let ht=proj.val[u1][this.u]['html'];let f=qq.reg.get('htmltotable');let d=qq.reg.get('panels').propertes;f(ht,d);}};//alert(f);
 for(let u in proj.val[u1])
 {let el0=qq.ce({tag:'div',style:{border:'1px solid black',background:'lightgray'},event:{click:ff},it:u,parent:el});el0.u=u;}
 }
