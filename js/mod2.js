a525b7df07dbcc14d7354cd9c7320eb8a3627a31
slach <pser@mail.ru>
2024-07-04 18:34:41 +0500

    qq file

--- js/mod2.js
+++ js/mod2.js
@@ -1,4 +1,4 @@
 
 export let mm=455
-alert(33);alert(window.a);
-import {sayHi} from 'https://slachinov.github.io/js/mod.js';alert(sayHi); sayHi('John');
+alert(33);//alert(window.a);
+import {sayHi} from 'https://slachinov.github.io/js/mod.js';/%alert(sayHi); sayHi('John');
