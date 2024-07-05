b726a76e837a9964721176f53825c06a2547f5dc
slach <pser@mail.ru>
2024-07-05 11:30:00 +0500

    qq file

--- js/mod5.js
+++ js/mod5.js
@@ -1,5 +1,5 @@
 
-export let mm=4543;
-alert(mm);
+export let dd=4543;
+alert(dd);
 //alert(33);//alert(window.a);
-setTimeout (function(){alert(222);alert(mm)},10000);
+setTimeout (function(){return;alert(222);alert(mm)},10000);
