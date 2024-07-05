a6f6b6fc0640444a7feb436cc3b9b758a9b0d1eb
slach <pser@mail.ru>
2024-07-05 11:21:44 +0500

    qq file

--- js/mod3.js
+++ js/mod3.js
@@ -1,5 +1,5 @@
-alert('mod3');
 
-
-export function qqf(user) {
-  alert(`это куку, ${user}!`);}
+export let mm={a:4543,b:'trt'};
+alert(mm);
+//alert(33);//alert(window.a);
+setTimeout (function(){return;alert(222);alert(mm);mm=44;alert(mm)},10000);
