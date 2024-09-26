1723a83c32ab4635028924f3adcf1fdc4f923266
slach <pser@mail.ru>
2024-09-26 17:48:09 +0500

    qq file

---
+++ js/panel3.v3.js
@@ -0,0 +1,23 @@
+
+
+alert(0);
+//import {main} from('main.js');
+let main={};
+alert(1);
+{alert(5);alert(main);
+let t=qq.ce({ qclass:'abs div zi(5) t(0) l(0) r(0) bg(yellow)',it:'top'});
+main.top=t;
+
+let b=qq.ce({qclass:'abs div zi(5) b(0) l(0) r(0) bg(gray)',it:'bottom'});
+
+let m=qq.ce({ qclass:'abs div zi(5) l(0) r(0) bg(blue)',style:{}});
+//>>>>>>
+const observer = new ResizeObserver(function(entries) {
+m.style.bottom=b.offsetHeight+'px';
+m.style.top=t.offsetHeight +'px';
+});
+observer.observe(b);
+observer.observe(t)
+
+};
+//export {t,m,b}
