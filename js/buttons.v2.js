77fd8948bc589b7b3d5415739d93141f01920ec6
slach <pser@mail.ru>
2024-09-26 21:58:44 +0500

    qq file

--- js/buttons.v2.js
+++ js/buttons.v2.js
@@ -1,5 +1,5 @@
 import {b} from '/js/panel3.v7.js';
 import {main} from '/js/main.js';
-alert('m='+main);
+//alert('m='+main);
 //alert(5);alert(b);
 let t=qq.ce({tag:'button', qclass:'bg(white) fs(25)',it:'localStorage',parent:b});
