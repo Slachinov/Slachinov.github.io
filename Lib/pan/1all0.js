9e6a04026458ad040b3e90694090907bd7c9592c
slach <pser@mail.ru>
2025-05-17 14:16:54 +0500

    qq file

---
+++ Lib/pan/1all0.js
@@ -0,0 +1,22 @@
+/**
+ * Создает и возвращает панель, занимающую всю область видимости браузера, с заданными стилями и добавляет в указанный родительский элемент (по умолчанию в document.body).
+ *
+ * @param {object} params - Объект с CSS-свойствами и (необязательно) parent для панели.
+ * Например: { 'background-color': 'lightgray', 'z-index': '0', parent: document.getElementById('someId') }.
+ * @returns {HTMLElement} - Созданный HTML-элемент div.
+ */
+export function createFullscreenPanel(params) {
+  const skin = {
+    style: {
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      width: '100%',
+      height: '100%',
+      ...params
+    },
+    parent: params.parent
+  };
+  return window.qq.ce({ skin: skin, tag:'div'});
+}
+
