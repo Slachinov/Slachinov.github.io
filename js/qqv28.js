134418d1f756a344a3cfd510294f85fdc11fefca
slach <pser@mail.ru>
2025-05-23 20:12:27 +0500

    qq file

---
+++ js/qqv28.js
@@ -0,0 +1,181 @@
+window.qq = {};
+qq.f = {};
+
+
+qq.f.isObject = function(o) {
+  return o && typeof o === 'object' && o.toString() === '[object Object]';
+};
+
+
+qq.f.extend = function(destination, source) {
+  for (let property in source) {
+    if (qq.f.isObject(source[property])) {
+      if (!destination[property] || !qq.f.isObject(destination[property])) {
+        destination[property] = {};
+      }
+      qq.f.extend(destination[property], source[property]);
+    } else {
+      destination[property] = source[property];
+    }
+  }
+  return destination;
+};
+
+
+qq.f.getStyle = function(style, elem) {
+  return document.defaultView.getComputedStyle(elem, null).getPropertyValue(style);
+};
+
+
+qq.f.createSkin = function() {
+  let skin = {};
+  let m = arguments[0];
+  let i0 = qq.f.isObject(m[0]) ? 0 : 1;
+  for (let i = i0; i < m.length; i++) {
+    qq.f.extend(skin, m[i]);
+  }
+  return skin;
+};
+
+
+qq.f.crElem = function() {
+  let skin = qq.f.createSkin(arguments);
+  let el;
+  let p = 0;
+
+
+  if (qq.f.isObject(arguments[0])) {
+    el = document.createElement(skin.tag || 'div');
+  } else {
+    el = arguments[0];
+    p = 1;
+  }
+
+
+  let eventBase = {
+    prop: {
+      hide: function() { this.style.display = 'none'; },
+      show: function() { this.style.display = 'block'; },
+      toggle: function() {
+        this.style.display = this.style.display !== 'none' ? 'none' : 'block';
+      },
+      getStyle: function(style) {
+        return qq.f.getStyle(style, this);
+      }
+    }
+  };
+
+
+  qq.f.extend(skin, eventBase);
+
+
+  if (skin.style) for (let u in skin.style) el.style[u] = skin.style[u];
+  if (skin.attr) for (let u in skin.attr) el.setAttribute(u, skin.attr[u]);
+  if (skin.class) el.className = skin.class;
+  if (skin.ih) el.innerHTML = skin.ih;
+  if (skin.it) el.innerText = skin.it;
+  if (skin.val) el.value = skin.val;
+  if (skin.prop) for (let u in skin.prop) el[u] = skin.prop[u];
+
+
+  if (skin.event) {
+    for (let u in skin.event) {
+      if (["longtap", "press", "swipeup", "swipedown", "swipeleft", "swiperight"].includes(u)) {
+        qq.f._gesture(el, u, skin.event[u]);
+      } else {
+        el.addEventListener(u, skin.event[u], false);
+      }
+    }
+  }
+
+
+  if (!p) {
+    let par;
+    if ((skin.parent !== 'none') && !skin.insb && !skin.insa) {
+      par = skin.parent || document.body;
+      par.appendChild(el);
+    }
+    if (skin.insb) {
+      let elb = skin.insb;
+      par = elb.parentNode;
+      par.insertBefore(el, elb);
+    }
+    if (skin.insa) {
+      par = skin.insa.parentNode;
+      let elb = skin.insa.nextSibling;
+      if (elb) par.insertBefore(el, elb);
+      else par.appendChild(el);
+    }
+    el.parent = par;
+  }
+
+
+  if (skin.init) skin.init(el);
+
+
+  el.skin = skin;
+  return el;
+};
+
+
+qq.f._gesture = function(el, type, handler) {
+  let timer, startX, startY, isMoving = false;
+  const threshold = 150;
+  el.style.touchAction = 'none';
+
+
+  el.addEventListener('pointerdown', e => {
+    startX = e.clientX;
+    startY = e.clientY;
+    isMoving = false;
+    if (type === 'press') {
+      timer = setTimeout(() => handler.call(el, e), 100);
+    } else if (type === 'longtap') {
+      timer = setTimeout(() => handler.call(el, e), 500);
+    }
+  });
+
+
+  el.addEventListener('pointermove', e => {
+    let dx = e.clientX - startX;
+    let dy = e.clientY - startY;
+    if (Math.abs(dx) > 10 || Math.abs(dy) > 10) {
+      isMoving = true;
+      clearTimeout(timer);
+    }
+  });
+
+
+  el.addEventListener('pointerup', e => {
+    clearTimeout(timer);
+    if (!isMoving && (type === 'press' || type === 'longtap')) return;
+
+
+    let dx = e.clientX - startX;
+    let dy = e.clientY - startY;
+
+
+    if (Math.abs(dx) > Math.abs(dy)) {
+      if (dx > threshold && type === 'swiperight') handler.call(el, e);
+      else if (dx < -threshold && type === 'swipeleft') handler.call(el, e);
+    } else {
+      if (dy > threshold && type === 'swipedown') handler.call(el, e);
+      else if (dy < -threshold && type === 'swipeup') handler.call(el, e);
+    }
+  });
+};
+
+
+qq.loadscript = function(url) {
+  return new Promise(function(resolve, reject) {
+    let el = document.createElement('script');
+    el.src = url;
+    el.addEventListener('load', () => resolve(url), false);
+    el.addEventListener('error', e => reject(e), false);
+    document.head.appendChild(el);
+  });
+};
+
+
+qq.ce = qq.f.crElem;
+qq.cs = qq.f.createSkin;
