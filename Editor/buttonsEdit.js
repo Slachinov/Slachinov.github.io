ab9ecf01fd327a5527aa33d05ca3f90e7cad85e5
slach <pser@mail.ru>
2026-01-29 17:14:01 +0500

    qq file

--- Editor/buttonsEdit.js
+++ Editor/buttonsEdit.js
@@ -1,10 +1,4 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <script src="js/qqv34.js"></script>
-  <script 
+
 import {m,butonsEditPanel,bottomPanel} from 'https://slachinov.github.io/Lib/pan/main.js';
 
 
