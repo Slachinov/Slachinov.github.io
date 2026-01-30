13a2e6756fc89ffd7b3edb894bc665be8cd46d7b
slach <pser@mail.ru>
2026-01-30 18:21:55 +0500

    qq file

--- Lib/pan/1all1.js
+++ Lib/pan/1all1.js
@@ -14,7 +14,7 @@ export function createFullscreenPanel(params) {
       width: '100%',
       height: '100%',
      background:'lightgray',
-     overflou:'auto'
+     overflow:'auto'
     }
   };
   return qq.ce(params,skin);
